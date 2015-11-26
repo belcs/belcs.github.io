c4f1b30d61181f795224aa041fc3a0c70f7e6c37
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2015-11-26 12:30:31 +0400

    samples

---
+++ samples/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int a = 10;
+        int i = 0;
+        while(i <= a){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
